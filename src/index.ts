de9573ae351e8c9ea0f134055d5da2f854a6fb8f
Ričardas Brazdžius <ricardas.brazdzius@gmail.com>
2025-05-10 09:10:54 +0300

    add MongoDB connection and integrate it into the application

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { type Request, type Response } from "express";
 import "@dotenvx/dotenvx/config";
 import { app } from "config/app";
+import { connectToDatabase } from "config/db";
 
 const port = process.env.PORT ?? 8000;
+
+connectToDatabase();
 
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json("Hello World!");
